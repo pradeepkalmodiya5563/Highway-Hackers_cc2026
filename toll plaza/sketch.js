6416a17bcbc22f982aefd931d38061a25bdfb9a5
pradeepkalmodiya5563 <pradeep.kalmodiya@avantika.edu.in>
2026-05-13 21:29:50 +0000

    starting

--- toll plaza/sketch.js
+++ toll plaza/sketch.js
@@ -1,310 +1,66 @@
- let angle=0; 
+ 
+    // =======================
+// TOLL PLAZA SYSTEM
+// =======================
+
+let laneAngles = {
+  100: 0,
+  200: 0,
+  300: 0,
+  400: 0,
+  500: 0,
+  600: 0
+};
+
+let activeMessage = {
+  text: "",
+  x: 0,
+  y: 0,
+  timer: 0
+};
+
+let vehicles = [];
+
+let vehicleSelect;
+let laneSelect;
+let startButton;
 
 function setup() {
-  createCanvas(800, 800);
-}
 
-function draw() {
-  background(150);
-  //first lane
-  fill(255,255,0);
-  rect(180,200,40,20);
-   fill(0);
-  rect(180,220,40,20);
-  fill(255,255,0);
-   rect(180,240,40,20);
-  fill(0);
-   rect(180,260,40,20);
-  fill(255,255,0);
-   rect(180,280,40,20);
-  fill(0);
-   rect(180,300,40,20);
-  fill(255,255,0);
-   //rect(180,320,40,20);
-  fill(0);
-   //rect(180,340,40,20);
-  fill(255,255,0);
-   rect(180,360,40,20);
-  fill(0);
-   rect(180,380,40,20);
-  fill(255,255,0);
-   rect(180,400,40,20);
-  fill(0);
-   rect(180,420,40,20);
-  fill(255,255,0);
-   rect(180,440,40,20);
-  fill(0);
-   rect(180,460,40,20); 
-  fill(255,255,0);
-   rect(180,480,40,20);
-  triangle(200,150,180,200,220,200);
-  triangle(200,550,180,500,220,500);
+  // FULL SCREEN RESIZE CANVAS
+  createCanvas(
+    windowWidth,
+    windowHeight
+  );
 
-   //second lane
-  fill(255,255,0);
-  rect(280,200,40,20);
-   fill(0);
-  rect(280,220,40,20);
-  fill(255,255,0);
-   rect(280,240,40,20);
-  fill(0);
-   rect(280,260,40,20);
-  fill(255,255,0);
-   rect(280,280,40,20);
-  fill(0);
-   rect(280,300,40,20);
-  fill(255,255,0);
-   //rect(280,320,40,20);
-  fill(0);
-   //rect(280,340,40,20);
-  fill(255,255,0);
-   rect(280,360,40,20);
-  fill(0);
-   rect(280,380,40,20);
-  fill(255,255,0);
-   rect(280,400,40,20);
-  fill(0);
-   rect(280,420,40,20);
-  fill(255,255,0);
-   rect(280,440,40,20);
-  fill(0);
-   rect(280,460,40,20); 
-  fill(255,255,0);
-   rect(280,480,40,20);
-  triangle(300,150,280,200,320,200);
-  triangle(300,550,280,500,320,500);
-  
-  
-  
- //fourth lane  
-  fill(255,255,0);
-  rect(480,200,40,20);
-   fill(0);
-  rect(480,220,40,20);
-  fill(255,255,0);
-   rect(480,240,40,20);
-  fill(0);
-   rect(480,260,40,20);
-  fill(255,255,0);
-   rect(480,280,40,20);
-  fill(0);
-   rect(480,300,40,20);
-  fill(255,255,0);
-   //rect(480,320,40,20);
-  fill(0);
-   //rect(480,340,40,20);
-  fill(255,255,0);
-   rect(480,360,40,20);
-  fill(0);
-   rect(480,380,40,20);
-  fill(255,255,0);
-   rect(480,400,40,20);
-  fill(0);
-   rect(480,420,40,20);
-  fill(255,255,0);
-   rect(480,440,40,20);
-  fill(0);
-   rect(480,460,40,20); 
-  fill(255,255,0);
-   rect(480,480,40,20);
-  triangle(500,150,480,200,520,200);
-  triangle(500,550,480,500,520,500);
+  // VEHICLE SELECT
+  vehicleSelect = createSelect();
+  vehicleSelect.position(20, 20);
 
-    //third lane
-  fill(255,255,0);
-  rect(380,200,40,20);
-   fill(0);
-  rect(380,220,40,20);
-  fill(255,255,0);
-   rect(380,240,40,20);
-  fill(0);
-   rect(380,260,40,20);
-  fill(255,255,0);
-   rect(380,280,40,20);
-  fill(0);
-   rect(380,300,40,20);
-  fill(255,255,0);
-   //rect(380,320,40,20);
-  fill(0);
-   //rect(380,340,40,20);
-  fill(255,255,0);
-   rect(380,360,40,20);
-  fill(0);
-   rect(380,380,40,20);
-  fill(255,255,0);
-   rect(380,400,40,20);
-  fill(0);
-   rect(380,420,40,20);
-  fill(255,255,0);
-   rect(380,440,40,20);
-  fill(0);
-   rect(380,460,40,20); 
-  fill(255,255,0);
-   rect(380,480,40,20);
-  triangle(400,150,380,200,420,200);
-  triangle(400,550,420,500,380,500);
-  
-  //fifth lane
-   fill(255,255,0);
-  rect(580,200,40,20);
-   fill(0);
-  rect(580,220,40,20);
-  fill(255,255,0);
-   rect(580,240,40,20);
-  fill(0);
-   rect(580,260,40,20);
-  fill(255,255,0);
-   rect(580,280,40,20);
-  fill(0);
-   rect(580,300,40,20);
-  fill(255,255,0);
-   //rect(580,320,40,20);
-  fill(0);
-   //rect(580,340,40,20);
-  fill(255,255,0);
-   rect(580,360,40,20);
-  fill(0);
-   rect(580,380,40,20);
-  fill(255,255,0);
-   rect(580,400,40,20);
-  fill(0);
-   rect(580,420,40,20);
-  fill(255,255,0);
-   rect(580,440,40,20);
-  fill(0);
-   rect(580,460,40,20); 
-  fill(255,255,0);
-   rect(580,480,40,20);
-  triangle(600,150,580,200,620,200);
-  triangle(600,550,580,500,620,500);
-  
-//toll office 
-  
-  fill(220);
-  rect(180,320,40,40);
-  fill(220);
-  rect(280,320,40,40);
-  fill(220);
-  rect(380,320,40,40);
-  fill(220);
-  rect(480,320,40,40);
-  fill(220);
-  rect(580,320,40,40);
-  
-  //barricade
-  //fill(255,0,0);
-  //rect(200,310,-70,5);
-  //rect(300,310,-70,5);
-  //rect(400,310,-70,5);
-  //rect(500,310,-70,5);
-  
-    // barricade 1
-push();
-translate(200,310);
-rotate(radians(angle));
-fill(255,0,0);
-rect(-70,0,70,5);
-pop();
+  vehicleSelect.option("car");
+  vehicleSelect.option("bus");
+  vehicleSelect.option("truck");
+  vehicleSelect.option("bike");
 
-// barricade 2
-push();
-translate(300,310);
-rotate(radians(angle));
-fill(255,0,0);
-rect(-70,0,70,5);
-pop();
+  // LANE SELECT
+  laneSelect = createSelect();
+  laneSelect.position(150, 20);
 
-// barricade 3
-push();
-translate(400,310);
-rotate(radians(angle));
-fill(255,0,0);
-rect(-70,0,70,5);
-pop();
+  laneSelect.option("200");
+  laneSelect.option("300");
+  laneSelect.option("400");
+  laneSelect.option("500");
+  laneSelect.option("600");
 
-// barricade 4
-push();
-translate(500,310);
-rotate(radians(angle));
-fill(255,0,0);
-rect(-70,0,70,5);
-pop();
-  
-  //barricade5
-  push();
-translate(600,310);
-rotate(radians(angle));
-fill(255,0,0);
-rect(-70,0,70,5);
-pop();
+  // BIKE AUTO LANE
+  vehicleSelect.changed(changeLaneForBike);
 
- // lane line
-   //stroke(255);
-  //strokeWeight(4);
-  line(200,150,200,130);
-  line(200,110,200,90);
-  line(200,70,200,50);
-  line(200,30,200,10);
-  
-  
-  line(300,150,300,130);
-  line(300,110,300,90);
-  line(300,70,300,50);
-  line(300,30,300,10);
-   
-  line(400,150,400,130);
-  line(400,110,400,90);
-  line(400,70,400,50);
-  line(400,30,400,10);
-   
-  line(500,150,500,130);
-  line(500,110,500,90);
-  line(500,70,500,50);
-  line(500,30,500,10);
-   
-  line(600,150,600,130);
-  line(600,110,600,90);
-  line(600,70,600,50);
-  line(600,30,600,10);
-  
-  line(200,550,200,570);
-  line(200,590,200,610);
-  line(200,630,200,650);
-  line(200,670,200,690);
-  
-  line(300,550,300,570);
-  line(300,590,300,610);
-  line(300,630,300,650);
-  line(300,670,300,690);
-   
-  line(400,550,400,570);
-  line(400,590,400,610);
-  line(400,630,400,650);
-  line(400,670,400,690);
-   
-  line(500,550,500,570);
-  line(500,590,500,610);
-  line(500,630,500,650);
-  line(500,670,500,690);
-   
-  line(600,550,600,570);
-  line(600,590,600,610);
-  line(600,630,600,650);
-  line(600,670,600,690);
-  
-  
-   
-  
-}
-
-function keyPressed(){
-
-  if(key === ' '){
-    angle = 80;
+  // START BUTTON
+  startButton =
+    createButton(
+      "START VEHICLE"
+    );
   }
-
-}
-   
   
 
     
